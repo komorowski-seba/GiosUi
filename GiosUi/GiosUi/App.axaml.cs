9af016f11182f9a34300be90afde21a21ee11a47
seba.kom <sebastian.komorowski@onwelo.com>
2026-02-12 15:44:26 +0100

    UI view

--- GiosUi/GiosUi/App.axaml.cs
+++ GiosUi/GiosUi/App.axaml.cs
@@ -1,11 +1,14 @@
+using System;
 using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Data.Core.Plugins;
 using System.Linq;
 using Avalonia.Markup.Xaml;
+using Core.Application.Extensions;
 using GiosUi.Services;
 using GiosUi.ViewModels;
 using GiosUi.Views;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace GiosUi;
 
@@ -20,22 +23,65 @@ public partial class App : Application
 
     public override void OnFrameworkInitializationCompleted()
     {
-        if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
+        _windowService.CreateScope((services) =>
         {
-            // Avoid duplicate validations from both Avalonia and the CommunityToolkit. 
-            // More info: https://docs.avaloniaui.net/docs/guides/development-guides/data-validation#manage-validationplugins
-            DisableAvaloniaDataAnnotationValidation();
-            desktop.MainWindow = new MainWindow
+            services.AddScoped<MainWindow>();
+       
+            services.AddTransient<DataSourceView>();
+            services.AddTransient<UpdatingFilesView>();
+       
+            services.AddTransient<MainViewModel>();
+            services.AddTransient<DataSourceViewModel>();
+            services.AddTransient<UpdatingFilesViewModel>();
+       
+            services.AddApplication();
+            // services.AddInfrastructure();            
+        });
+
+        /*
+            var showItemDataSource = new NativeMenuItem("Pokaż okno - data source ");
+            showItemDataSource.Click += (_, _) =>
             {
-                DataContext = new MainViewModel()
+                desktop.MainWindow = _windowService.Show<DataSourceViewModel>();
             };
-        }
-        else if (ApplicationLifetime is ISingleViewApplicationLifetime singleViewPlatform)
+
+            var showItemUpdateFiles = new NativeMenuItem("Pokaż okno - update files ");
+            showItemUpdateFiles.Click += (_, _) =>
+            {
+                desktop.MainWindow = _windowService.Show<UpdatingFilesViewModel>();
+            };
+            
+            var exitItem = new NativeMenuItem("Zakończ");
+            exitItem.Click += (_, _) => desktop.Shutdown();
+        */
+        
+        // if (ApplicationLifetime is ISingleViewApplicationLifetime singleView)
+        // {
+        //     // singleView.
+        //     //
+        //     // singleView.MainView = new MainView { DataContext = new MainViewModel() };
+        // }        
+        
+        switch (ApplicationLifetime)
         {
-            singleViewPlatform.MainView = new MainView
-            {
-                DataContext = new MainViewModel()
-            };
+            case ISingleViewApplicationLifetime singleViewPlatform:
+                Console.WriteLine(" === >> here");
+                singleViewPlatform.MainView = _windowService.Show<DataSourceViewModel>();
+                // singleViewPlatform.MainView = new MainView
+                // {
+                //     DataContext = new MainViewModel()
+                // };
+                break;
+            
+            case IClassicDesktopStyleApplicationLifetime desktop:
+                // Avoid duplicate validations from both Avalonia and the CommunityToolkit. 
+                // More info: https://docs.avaloniaui.net/docs/guides/development-guides/data-validation#manage-validationplugins
+                DisableAvaloniaDataAnnotationValidation();
+                desktop.MainWindow = new MainWindow
+                {
+                    DataContext = new MainViewModel()
+                };
+                break;
         }
 
         base.OnFrameworkInitializationCompleted();

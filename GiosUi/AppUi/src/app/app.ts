67c37e4a589f0f1125e564be6d922e92546bfebe
seba <komorowski.seba@gmail.com>
2026-03-09 22:48:39 +0100

    taiga ui

--- GiosUi/AppUi/src/app/app.ts
+++ GiosUi/AppUi/src/app/app.ts
@@ -1,13 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TuiRoot } from '@taiga-ui/core';
+import { TuiDrawer } from '@taiga-ui/layout';
 
 import { MenuComponent } from './view/menu/menu.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MenuComponent, TuiRoot],
+  imports: [RouterOutlet, MenuComponent, TuiRoot, TuiDrawer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })

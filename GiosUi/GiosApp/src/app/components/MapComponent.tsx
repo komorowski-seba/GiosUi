f953a4f2578f46d2a861cd2a6b38e7033f475a02
seba <komorowski.seba@gmail.com>
2026-04-09 23:59:53 +0200

    markers

--- GiosUi/GiosApp/src/app/components/MapComponent.tsx
+++ GiosUi/GiosApp/src/app/components/MapComponent.tsx
@@ -12,44 +12,56 @@ const defaultIcon = L.icon({
 
 L.Marker.prototype.options.icon = defaultIcon;
 
-const position: LatLngExpression = [52.2297, 21.0122];
-
-export const MapComponent = () => {
-    return (
-        <MapContainer
-            center={position}
-            zoom={13}
-            style={{ height: '400px', width: '100%' }}
-        >
-            <TileLayer
-                attribution='&copy; OpenStreetMap contributors'
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            />
-
-            <Marker position={position}>
-                <Popup>
-                    Hej 👋 <br /> To jest Warszawa!
-                </Popup>
-            </Marker>
-        </MapContainer>
-    );
-};
-
 export interface MapMarker {
     id: string;
     name: string;
     position: [number, number];
 };
 
+type MapProps = {
+    markers: MapMarker[];
+    onMarkerClick: (marker: MapMarker) => void;
+};
+
+export const MapComponent = ({markers, onMarkerClick}: MapProps) => {
+    return (
+        <MapContainer
+            center={position}
+            zoom={13}
+            style={{ height: '100%', width: '100%' }}
+        >
+            <TileLayer
+                attribution='&copy; OpenStreetMap contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+
+            {markers.map((marker) => (
+                <Marker
+                    key={marker.id}
+                    position={marker.position}
+                    eventHandlers={{
+                        click: () => onMarkerClick(marker),
+                    }}
+                >
+                    <Popup>{marker.name}</Popup>
+                </Marker>
+            ))}
+            
+        </MapContainer>
+    );
+};
+
 /*
+
+            <Marker position={position}>
+                <Popup>
+                    Hej 👋 <br /> To jest Warszawa!
+                </Popup>
+            </Marker>
+
 
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import { MapMarker } from "./types";
-
-type Props = {
-  markers: MapMarker[];
-  onMarkerClick: (marker: MapMarker) => void;
-};
 
 export const MapView = ({ markers, onMarkerClick }: Props) => {
   return (
--- GiosUi/GiosApp/src/app/views/MapView.tsx
+++ GiosUi/GiosApp/src/app/views/MapView.tsx
@@ -3,14 +3,21 @@ import { useDispatch } from 'react-redux';
 import { DrawerComponent } from '../components/DrawerComponent';
 import { setDrawerOpen } from '../stores/reducer';
 import { MapComponent } from '../components/MapComponent';
-
+import type { MapMarker } from '../components/MapComponent';
 
 export function MapView() {
     const dispatch = useDispatch();
+    const markers: MapMarker[] = [
+        { id: '1', name: '11', position: [52.2297, 21.0122] },
+        { id: '2', name: '22', position: [52.2197, 21.0222] },
+    ]
+
+    const handleMarkerClick = (marker: MapMarker): void => {
+        console.log('marker: ', marker.id);
+    };
 
     const handleClick = () => {
         dispatch(setDrawerOpen(true));
-        console.log(" ===> show drawer")
     };
     
     return (
@@ -20,7 +27,7 @@ export function MapView() {
                 Pokaż maps szczegóły
             </Button>
             <Card title="Mapa">
-                <MapComponent />
+                <MapComponent markers={markers} onMarkerClick = {handleMarkerClick} />
             </Card>
             <DrawerComponent />
         </>
@@ -32,11 +39,6 @@ export function MapView() {
 import { MapView } from "./MapView";
 
 function App() {
-
-  const handleMarkerClick = (marker: any) => {
-    console.log("Kliknięto marker:", marker);
-    alert(marker.name);
-  };
 
   return (
     <MapView

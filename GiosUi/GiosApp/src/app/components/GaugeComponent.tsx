2c1c86a8788dec98c89340d81069b69d13356c55
seba <komorowski.seba@gmail.com>
2026-04-22 23:40:25 +0200

    gauge component

---
+++ GiosUi/GiosApp/src/app/components/GaugeComponent.tsx
@@ -0,0 +1,40 @@
+import { PieChart, Pie, Cell } from 'recharts';
+
+const getColor: string = (value: number) => {
+    if (value < 60) return '#52c41a';
+    if (value < 80) return '#faad14';
+    return '#ff4d4f';
+};
+
+type GaugeComponentProps = { value: number; name: string };
+
+const GaugeComponent: Element = ({ value, name }: GaugeComponentProps) => {
+    const data = [
+        { name: 'value', value },
+        { name: 'rest', value: 100 - value },
+    ];
+
+    const color = getColor(value);
+
+    return (
+        <div style={{ textAlign: 'center' }}>
+            <PieChart width={200} height={140}>
+                <Pie
+                    data={data}
+                    startAngle={180}
+                    endAngle={0}
+                    innerRadius={60}
+                    outerRadius={80}
+                    dataKey="value"
+                >
+                    <Cell fill={color} />
+                    <Cell fill="#eee" />
+                </Pie>
+            </Pie>
+
+            <div style={{ marginTop: -10, fontWeight: 600 }}>
+                {name}: {value}%
+            </div>
+        </div>
+    );
+};
--- GiosUi/GiosApp/src/app/views/ListView.tsx
+++ GiosUi/GiosApp/src/app/views/ListView.tsx
@@ -1,7 +1,7 @@
 import { Button } from 'antd';
 import { useDispatch } from 'react-redux';
 
-export function ListView() {
+export function ListView(): Element {
     const dispatch = useDispatch();
 
     const handleClick = () => {
